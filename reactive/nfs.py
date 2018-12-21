1f7bc060782ffc4f83065df1ab55bd2801ff1de2
Colin Watson <cjwatson@canonical.com>
2018-12-21 16:55:53 +0000

    Add active_units configuration
    Assuming that the requiring charms support it, this allows reasonably
    graceful migration from one NFS unit to another, along the lines of the
    following procedure:
    
      $ juju config nfs active_units=<old unit ID>
      $ juju add-unit nfs
      $ juju config nfs active_units=none
      # wait for clients to unmount, then move storage to new unit
      $ juju config nfs active_units=<new unit ID>
      # wait for clients to mount
      $ juju remove-unit <old unit ID>
      $ juju config nfs active_units=
    
    This involves moving publishing data on the mount relation to
    `read_nfs_mounts`, which seems more correct anyway (we shouldn't publish
    a mount until it has been exported).

--- reactive/nfs.py
+++ reactive/nfs.py
@@ -2,8 +2,12 @@ from collections import defaultdict
 import os
 
 from charmhelpers.core import hookenv
-from charmhelpers.core.host import service_start, service_running
-from charmhelpers.core.host import service_restart
+from charmhelpers.core.host import (
+    service_restart,
+    service_running,
+    service_start,
+    service_stop,
+)
 from charmhelpers.fetch import apt_install
 from charms.reactive import when, when_not, when_any, set_flag, clear_flag
 from charms.reactive.relations import endpoint_from_flag
@@ -50,15 +54,21 @@ def update_config():
         service_restart('nfs-kernel-server')
 
 
-@when('refresh_nfs_mounts')
+@when_any('refresh_nfs_mounts',
+          'config.changed.mount_options',
+          'config.changed.active_units')
 def read_nfs_mounts():
+    mount_interface = endpoint_from_flag('endpoint.nfs.joined')
+    if mount_interface is None:
+        hookenv.log('No mount interface, bailing')
+        return
+
     hookenv.status_set('maintenance', 'Updating NFS mounts')
     if service_running('nfs-kernel-server'):
         try:
             command = ['exportfs', '-ra']
             hookenv.log('Executing {}'.format(command))
             check_output(command)
-            clear_flag('refresh_nfs_mounts')
         except CalledProcessError as e:
             hookenv.log(e)
             hookenv.log('Failed to reread nfs mounts. Will attempt again next update.')  # noqa
@@ -70,6 +80,38 @@ def read_nfs_mounts():
             hookenv.log(e)
             hookenv.log('Unable to start service nfs-kernel-server! Will attempt again next update.') # noqa
             return
+
+    config = hookenv.config()
+    active_units = config.get('active_units', '')
+    if not active_units or hookenv.local_unit() in active_units.split(','):
+        storage_root = config.get('storage_root')
+        mount_options = config.get('mount_options')
+        mount_responses = []
+        for mount in mount_interface.get_mount_requests():
+            if not mount['application_name']:
+                continue
+            path = os.path.join(storage_root, mount['application_name'])
+            mount_responses.append({
+                'export_name': mount['application_name'],
+                'mountpoint': path,
+                'identifier': mount['identifier'],
+                'fstype': 'nfs',
+                'options': mount_options,
+            })
+        mount_interface.configure(mount_responses)
+    else:
+        hookenv.log('Disabled (not in active_units)')
+        for relation in mount_interface.relations:
+            relation.to_publish_raw.update({
+                'mountpoint': None,
+                'hostname': None,
+                'fstype': None,
+                'options': None,
+            })
+        if service_running('nfs-kernel-server'):
+            service_stop('nfs-kernel-server')
+
+    clear_flag('refresh_nfs_mounts')
 
 
 @when_not('nfs.changed', 'refresh_nfs_mounts')
@@ -92,13 +134,11 @@ def nfs_relation_changed():
     config = hookenv.config()
     storage_root = config.get('storage_root')
     export_options = config.get('export_options')
-    mount_options = config.get('mount_options')
 
     # get desired mounts
     mount_list = mount_interface.get_mount_requests()
 
     mount_addresses = defaultdict(set)
-    mount_responses = []
 
     for mount in mount_list:
         if not mount['application_name']:
@@ -115,13 +155,6 @@ def nfs_relation_changed():
             os.chmod(path, 0o777)
 
         mount_addresses[path].update(mount['addresses'])
-        mount_responses.append({
-            'export_name': mount['application_name'],
-            'mountpoint': path,
-            'identifier': mount['identifier'],
-            'fstype': 'nfs',
-            'options': mount_options,
-        })
 
     if mount_addresses:
         template_context = {
@@ -137,4 +170,3 @@ def nfs_relation_changed():
         os.remove(EXPORT_FILENAME)
 
     set_flag('refresh_nfs_mounts')
-    mount_interface.configure(mount_responses)
